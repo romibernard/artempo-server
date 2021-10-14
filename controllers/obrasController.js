7563c36698a4685e9e06eb4db1c7349aa38cb74e
Romina <romibernard68@gmail.com>
2021-10-14 16:44:25 -0500

    controllers auth y user, fix de bcrypt

---
+++ controllers/obrasController.js
@@ -0,0 +1,1 @@
+const Obras = require('./../models/Obras')
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,19 +14,52 @@ exports.createUser = async (req, res) => {
         })
     }
 
-    // PEDIR LOS DATOS DEL FORMULARIO
-    const { username, email, password } = req.body
+    // Datos del formulario p/usuario
+    const { email, password } = req.body
 
-    // ENCRIPTACIÓN
+    // Encriptando contraseña
     try {
         const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password, salt)
         const newUser = await User.create({
-            username,
             email,
             hashedPassword
         })
         console.log(newUser)
-    }
+        //Registro ok-> no tengo que hacer login
+        //Autenticación - datos de la credencial:
+        const payload = {
+            user: {
+                id: newUser._id
+            }
+        }
+        // "firma" del servidor
+        jwt.sign(
+            payload, //datos de la credencial
+            process.env.SECRET, //firma de mi env
+            {
+                expiresIn: 360000 //"cookie"
+            },
 
+            //Si falla el ingreso...
+            (error, token) => {
+                console.log(error)
+                if (error) {
+                    return res.status(401).json({
+                        msgError: "Hubo un problema en la creación del token."
+                    })
+                }
+                return res.json({
+                    data: {
+                        token
+                    }
+                })
+            }
+        )
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msgError: "Hubo un problema creando el usuario."
+        })
+    }
 }
