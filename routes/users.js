5b5e0b793c8a2fcffa604395002dc50d03ab706a
Romina <romibernard68@gmail.com>
2021-10-17 01:49:07 -0500

    ruta de usuario fix

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,7 @@ const { check } = require("express-validator")
 const usersController = require("./../controllers/usersController")
 
 
-// CRUD DE USUARIOS
-// GET - USERS - OBTENER TODOS LOS USUARIOS PARA LOS ADMINS
-/*
-  1. SOLO LOS ADMINS PUEDEN ACCEDER A TODOS LOS USUARIOS
-  2. PEDIR AUTENTICACIÓN DE ADMIN
- */
-
-
-// Creacicón de usuario
+// Creación de usuario
 router.post("/create",
     //es un array porque debe hacer varias validaciones
     [                                               //.not  corrobora que una casilla no esté vacía
@@ -21,16 +13,6 @@ router.post("/create",
         check("password", "El password debe ser mínimo de 6 caracteres").isLength({ min: 6 })
     ]
     , usersController.createUser)
-
-
-//por corroborar si se anexará por cuestión de tiempo:
-
-// PUT - USER - ACTUALIZAR UN USUARIO
-//SOLO LOS MISMOS USUARIOS PUEDEN ACTUALIZAR SUS DATOS
-
-
-// DELETE - USER - BORRAR UN USUARIO
-//SOLO ADMINS PUEDEN BORRAR AL USUARIO
 
 
 module.exports = router
