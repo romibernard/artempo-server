be63929079c4a4b9948d7ce2146101c11718b646
Romina <romibernard68@gmail.com>
2021-10-18 23:53:24 -0500

    deploy

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.post("/create",
     //es un array porque debe hacer varias validaciones
     [                                               //.not  corrobora que una casilla no esté vacía
         check("username", "El nombre es obligatorio.").not().isEmpty(),
-        check("email", "Agrega un email válido").isEmail().not().isEmpty(),
+        check("email", "Agrega un email válido").isEmail(),
         check("password", "El password debe ser mínimo de 6 caracteres").isLength({ min: 6 }).not().isEmpty()
     ]
     , usersController.createUser)
